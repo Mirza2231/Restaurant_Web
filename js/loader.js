847e44377c4914304a1b6dc5bd7d49a377774294
Mirza2231 <mirzafaraz2290@gmail.com>
2024-01-18 09:22:02 -0800

    first commit

---
+++ js/loader.js
@@ -0,0 +1,11 @@
+function loader() {
+    document.querySelector(".loader-container").classList.add("fade-out")
+  }
+
+  function fadeOut() {
+    setInterval(loader, 4000)
+  }
+
+  window.onload = fadeOut();
+
+  
